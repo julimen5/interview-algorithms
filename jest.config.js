a53131361bf52589b0b30b82c70b959d804633bc
julian <julimendez5393@gmail.com>
2021-06-18 17:45:02 -0300

    coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
+  coverageDirectory: './coverage/',
   collectCoverage: true,
   modulePathIgnorePatterns: ['<rootDir>/spec/e2e'],
   restoreMocks: true,
