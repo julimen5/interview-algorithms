63acbac3420851460d38e13ff5dc32e10ce6794f
Julian Mendez <julian@selehann.com>
2021-11-08 23:46:32 -0300

    add jumpSearch

--- search/__tests__/search.spec.js
+++ search/__tests__/search.spec.js
@@ -12,4 +12,9 @@ describe('Testing search algorithms', () => {
     expect(search.binarySearch([1, 2, 3, 4, 5, 6], 7)).toBe(-1);
     done();
   });
+  it('should be founded by jumpSearch', function () {
+    expect(search.jumpSearch([1, 2, 3, 4, 5], 1, 4)).toBe(0);
+    expect(search.jumpSearch([1, 2, 3, 4, 5], 7,2)).toBe(-1);
+    expect(search.jumpSearch([1, 2, 3, 4, 5, 6], 4,1)).toBe(3);
+  });
 });
