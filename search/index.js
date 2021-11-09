329db22f380154dba18630e5182a8bc72fe628a4
Julian Mendez <julian@selehann.com>
2021-11-08 23:46:32 -0300

    add jumpSearch

--- search/index.js
+++ search/index.js
@@ -1,7 +1,9 @@
 const linearSearch = require('./linearSearch');
 const binarySearch = require('./binarySearch');
+const jumpSearch = require("./jumpSearch");
 
 module.exports = {
   ...linearSearch,
   ...binarySearch,
+  ...jumpSearch,
 };
